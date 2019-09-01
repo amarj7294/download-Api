eeecba0dca4cc8e861614584dbb3d38c30906740
c5260086 <amar.janardhanan@sap.com>
2019-09-01 23:24:33 +0530

    initial heroku setup

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def initial():
     return 'hi!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
